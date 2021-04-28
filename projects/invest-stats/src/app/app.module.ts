47c93a665ad21dc135ccafba076747af2db7b914
Janusz Sokołow <SpaceIT-JS@gmail.com>
2021-04-28 23:37:01 +0200

    #003 [invest-stats] ng generate store DataState --project=invest-stats --module app.module.ts

--- projects/invest-stats/src/app/app.module.ts
+++ projects/invest-stats/src/app/app.module.ts
@@ -6,8 +6,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { DataTableComponent } from './components/data-table/data-table.component';
+import * as fromDataState from './reducers';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,11 @@ import { DataTableComponent } from './components/data-table/data-table.component
     MatTableModule,
     MatInputModule,
     MatIconModule,
+    StoreModule.forFeature(
+      fromDataState.dataStateFeatureKey,
+      fromDataState.reducers,
+      { metaReducers: fromDataState.metaReducers }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
